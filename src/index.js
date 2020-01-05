2a8927435f77614636c6598ad28dff6dcf2c1a23
Ricky Adams <rickyadams432@gmail.com>
2020-01-05 00:35:33 -0800

    converted index.css to index.scss and now import bootstrap through index.scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
+import "./assets/sass/index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
