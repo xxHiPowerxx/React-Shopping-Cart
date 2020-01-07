9b94c90a6714a969e993300ba200cce41d2167c4
Ricky Adams <rickyadams432@gmail.com>
2020-01-06 20:05:14 -0800

    Updates

---
+++ precache-manifest.5ea7e547ce5c0699af2315dd7ca5e79f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c826636a49fff5539c1d19e5903600a2",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "1e30c3fcd3930202f83c",
+    "url": "/react-gh-pages/static/css/main.d035d9bd.chunk.css"
+  },
+  {
+    "revision": "458555fc36220d2e5ea4",
+    "url": "/react-gh-pages/static/js/2.46df2bb9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.46df2bb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e30c3fcd3930202f83c",
+    "url": "/react-gh-pages/static/js/main.f4cfdc09.chunk.js"
+  },
+  {
+    "revision": "9bbb23959c72422806a9",
+    "url": "/react-gh-pages/static/js/runtime-main.f8559acb.js"
+  }
+]);
